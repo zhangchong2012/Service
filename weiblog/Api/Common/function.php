53d671b1c0f1fd6f97cdc4e467adbba5da6dfeda
张充 <zchong@meizu.com>
2015-04-27 20:06:15 +0800

    [PHP服务器][服务]提交代码，和客户端链条一个接口成功

--- weiblog/Api/Common/function.php
+++ weiblog/Api/Common/function.php
@@ -2,3 +2,16 @@
 	function showErrorMsg($msg){
 		echo $msg;
 	}
+
+	function initDatabases(){
+		$hostName = C("DB_HOST");
+		$dbName = C("DB_NAME");
+		$dbPwd = C("DB_PWD");
+		$con = mysql_connect($hostName,$dbName,$dbPwd);
+		if (!$con){
+			  die('Could not connect: ' . mysql_error());
+		}
+
+		
+		mysql_close($con);
+	}
--- weiblog/Api/Controller/UserController.class.php
+++ weiblog/Api/Controller/UserController.class.php
@@ -5,30 +5,40 @@ use Think\Controller;
 class UserController extends Controller{
 	public function login(){
 		// echo I('get.');
-		print_r(I('get.'));
-		if(!IS_POST){
-			showErrorMsg("not support get params!");
-			return ;
-		}
+		// print_r(I('get.'));
+		// if(!IS_POST){
+		// 	showErrorMsg("not support get params!");
+		// 	return ;
+		// }
 			
 
-		showErrorMsg();
-		//跨模块调用
-		// A('Index')->showErrorMsg("get is not support!");
-		// $index = A('Index');
-		// $index->index();
+		// showErrorMsg();
+		// //跨模块调用
+		// // A('Index')->showErrorMsg("get is not support!");
+		// // $index = A('Index');
+		// // $index->index();
 
-		$login_name = I('post.login_name');
-		if(empty($login_name)){
-			echo "账户存在异常";
-			return ;
-		}
+		// $login_name = I('post.login_name');
+		// if(empty($login_name)){
+		// 	echo "账户存在异常";
+		// 	return ;
+		// }
 
-		$login_pwd = I('post.login_pwd');
-		if(empty($login_pwd)){
-			echo "密码存在异常";
-			return;
-		}
+		// $login_pwd = I('post.login_pwd');
+		// if(empty($login_pwd)){
+		// 	echo "密码存在异常";
+		// 	return;
+		// }
+	$arr = array(
+    'name' => '林则徐',
+    'nick' => 'haran',
+    'contact' => array(
+        'email' => 'url at qq dot com',
+        'website' => 'http://www.url.com',
+        )
+    );    
+	$json_string = json_encode($arr);
+	echo $json_string;
 
 		//数据库中检测账号和密码
 		
